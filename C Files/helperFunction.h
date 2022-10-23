825fa163309189911d7c46050f4f27793f1cd908
bismay <bismaypurkayastha5467@gmail.com>
2022-10-23 15:24:20 +0530

    Organised helper functions in a single file

---
+++ C Files/helperFunction.h
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<math.h>
+
+int* mallocOrDie(long size) {
+    
+    int* memAlloc = (int *)malloc(sizeof(int)*size*size);
+    
+    if(memAlloc==NULL) {
+        printf("\nCouldn't allocate memory\n");
+        exit(EXIT_FAILURE);
+    }
+
+    return memAlloc;
+}
+
+long calculateDimensions(long sizeOfArray) {
+    long dimensions = (long)ceil(sqrt(sizeOfArray));
+    return dimensions;
+}
+
+int* transpose(int* Matrix, int dimensions) {
+    
+    int *transposeMatrix = mallocOrDie(dimensions);
+
+    for(long i = 0; i < dimensions; i++) {
+        for(long j = 0; j < dimensions; j++) {
+            transposeMatrix[j*dimensions + i] = Matrix[i*dimensions + j];
+        }
+    }
+
+    return transposeMatrix;
+}
+
+int* xorOperation(int* Matrix, long dimensions, char* key) {
+    
+    for(long i = 0, k = 0; i < dimensions; i++) {
+        for(long j = 0; j < dimensions; j++) {
+            Matrix[i*dimensions + j] ^= key[k % strlen(key)];
+            k++;
+        }
+    }
+
+    return Matrix;
+
+}
