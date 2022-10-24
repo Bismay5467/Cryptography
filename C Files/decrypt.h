f9e02dac1be3d8631d27e0a826d03946a30c857a
bismay <bismaypurkayastha5467@gmail.com>
2022-10-24 06:29:44 +0530

    Total Decryption Done

--- C Files/decrypt.h
+++ C Files/decrypt.h
@@ -10,20 +10,67 @@
 
 #define DECRYPT 1
 
-int* decryption(int* asciiArray, long sizeOfArray, char* key, long* dimension) {
+int* arrayConversion(int* matrix, long dimension, long* sizeOfArray) {
+    
+    int flag = 0;
+    long posI, posJ;
 
-    *dimension = calculateDimensions(sizeOfArray);
-
-    int *decryptedArray = xorOperation(asciiArray, *dimension, key);
-    decryptedArray = transpose(decryptedArray, (int)dimension);
-    for(long i = 0; i < *dimension; i++){
-        for(long j = 0; j < *dimension; j++){
-            printf("|%d||%c|\t", decryptedArray[i*(*dimension) + j],decryptedArray[i*(*dimension) + j]);
+    for(long i = dimension-1; i >= 0; i--) {
+        for(long j = dimension-1; j >= 0; j--) {
+            if(matrix[i*dimension+j] == '^') {
+                flag = 1;
+                posJ = j;
+                break;
+            }
         }
-        printf("\n");
+        if(flag == 1) {
+            posI = i;
+            break;
+        }
     }
 
-    return NULL;
+    *sizeOfArray = (posI*dimension+posJ);
+    
+    int* asciiArray = (int*)malloc(sizeof(int)*(*sizeOfArray));
+    if(asciiArray == NULL) {
+        printf("\nCouldn't allocate memory\n");
+        exit(EXIT_FAILURE);
+    }
+
+    for(long i = posI, j = posJ, k = *sizeOfArray; i >= 0;) {
+        asciiArray[k--] = matrix[i*dimension+j];
+        j--;
+        if(j == -1) {
+            j = dimension-1;
+            i--;
+        }
+    }
+
+    return asciiArray;
+}
+
+int* reApplyKey(int* array, long sizeOfArray, char* key) {
+
+    long lengthOfkey = strlen(key);
+    
+    for(long i = 0, j = 0; i < sizeOfArray; i++, j++) {
+        array[i] = 256 - array[i] - key[j % lengthOfkey];
+    }
+
+    return array;
+}
+
+int* decryption(int* asciiArray, long *sizeOfArray, char* key, long* dimension) {
+
+    *dimension = calculateDimensions(*sizeOfArray);
+
+    int *decryptedArray = xorOperation(asciiArray, *dimension, key);
+    decryptedArray = transpose(decryptedArray, *dimension);
+    int* array = arrayConversion(decryptedArray, *dimension, sizeOfArray);
+    array = reApplyKey(array, *sizeOfArray, key);
+
+    free(decryptedArray);
+    return array;
 }
 
 void decrypt(char* sourceFile, char* destinationFile, char* key) {
@@ -32,11 +79,8 @@ void decrypt(char* sourceFile, char* destinationFile, char* key) {
     long dimension;
 
     int* asciiArray = readFile(sourceFile, &sizeOfArray); 
-    int* decryptedArray = decryption(asciiArray, sizeOfArray, key, &dimension);
-
-
-    
-    // writeFile(asciiArray, sizeOfArray, DECRYPT, destinationFile);
+    int* decryptedArray = decryption(asciiArray, &sizeOfArray, key, &dimension);    
+    writeFile(decryptedArray, sizeOfArray, DECRYPT, destinationFile);
 
     free(asciiArray);
     free(decryptedArray);
